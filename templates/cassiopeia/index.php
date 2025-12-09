f462f5179150c76baacc81d3e2b411ec7dbe1d14
antonio909 <78562889+antonio909@users.noreply.github.com>
2025-12-09 19:42:27 -0300

    Create index.php

---
+++ templates/cassiopeia/index.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * @package    Joomla.Site
+ * @subpackage Templates.cassiopeia
+ *
+ * @copyright  (C) 2017 Open Source Matters, Inc. <https://www.joomla.org>
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+defined('_JEXEC') or die;
+
+use Joomla\CMS\Fatory;
+use Joomla\CMS\HTML\HTMLHelper;
+use Joomla\CMS\Language\Text;
+use Joomla\CMS\Uri\Uri;
+
+/** @var Joomla\CMS\Document\HtmlDocument $this */
+
+$app   = Factory::getApplication();
+$input = $app->getInput();
+$wa    = $this->getWebAssetManager();
+
+// Browsers support SVG favicons
+$this->addHeadLink(HTMLHelper::_('image', 'joomla-favicon.svg', '', [], true, 1), 'icon', 'rel', ['type' => 'image/svg+xml']);
