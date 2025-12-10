8e5831a2ef17defe3a484bcffd425f2e9807645d
antonio909 <78562889+antonio909@users.noreply.github.com>
2025-12-09 23:59:30 -0300

    Update index.php

--- templates/cassiopeia/index.php
+++ templates/cassiopeia/index.php
@@ -23,3 +23,96 @@ $wa    = $this->getWebAssetManager();
 
 // Browsers support SVG favicons
 $this->addHeadLink(HTMLHelper::_('image', 'joomla-favicon.svg', '', [], true, 1), 'icon', 'rel', ['type' => 'image/svg+xml']);
+$this->addHeadLink(HTMLHelper::_('image', 'favicon.ico', '', [], true, 1), 'alternate icon', 'rel', ['type' => 'image/vnd.microsoft.icon']);
+$this->addHeadLink(HTMLHelper::_('image', 'joomla-favico-pinned.png', '', [], true, 1), 'mask-icon', 'rel', ['color' => '#000']);
+
+// Detecting Active Variables
+$option    = $input->getCmd('option', '');
+$view      = $input->getCmd('view', '');
+$layout    = $input->getCmd('layout', '');
+$task      = $input->getCmd('task', '');
+$itemid    = $input->getCmd('Itemid', '');
+$sitename  = htmlspecialchars($app->get('sitename'), ENT_QUOTES, 'UTF-8');
+$menu      = $app->getMenu()->getActive();
+$pageclass = $menu !== null ? $menu->getParams()->get('pageclass_sfx', '') : '';
+
+// Color Theme
+$paramsColorName = $this->params->get('colorName', 'colors_standard');
+$assetColorName  = 'theme.' . $paramsColorName;
+
+// Use a font scheme if set in the template style options
+$paramsFontScheme = $this->params->get('useFontScheme', false);
+$fontStyles       = '';
+
+if ($paramsFontScheme) {
+  if (stripos($paramsFontScheme, 'https://') === 0) {
+    $this->getPreloadManager()->preconnect('https://fonts.googleapis.com/', ['crossorigin' => 'anonymous']);
+    $this->getPreloadManager()->preconnect('https://fonts.gstatic.com/', ['crossorigin' => 'anonymous']);
+    $this->getPreloadManager()->preload($paramsFontScheme, ['as' => 'style', 'crossorigin' => 'anonymous']);
+    $wa->registerAndUseStyle('fontscheme.current', $paramsFontScheme, [], ['rel' => 'lazy-stylesheet', 'crossorigin' => 'anonymous']);
+
+    if (preg_match_all('/family=([^?:]*):/i', $paramsFontScheme, $matches) > 0) {
+      $fontStyles = '--cassiopeia-font-family-body: "' . str_replace('+', ' ', $matches[1][0]) . '", sans-serif; --cassiopeia-font-family-headings: "' . str_replace('+', ' ', $matches[1][1] ?? $matches[1][0]) . '", sans-serif; --cassiopeia-font-weight-normal: 400; --cassiopeia-font-weight-headings: 700;';
+    }
+  } elseif ($paramsFontScheme === 'system') {
+    $fontStylesBody    = $this->params->get('systemFontBody', '');
+    $fontStylesHeading = $this->params->get('systemFontHeading', '');
+
+    if ($fontStylesBody) {
+      $fontStyles = '--cassiopeia-font-family-body: "' . $fontStylesBody . '"; --cassiopeia-font-weight: 400;';
+    }
+    if ($fontStylesHeading) {
+      $fontStyles .= '--cassiopeia-font-family-headings: "' . $fontStylesHeading . '"; --cassiopeia-font-weight-headings: 700;';
+    }
+  } else {
+    $wa->registerAndUseStyle('fontscheme.current', $paramsFontScheme, ['version' => 'auto'], ['rel' => 'lazy-stylesheet']);
+    $this->getPreloadManager()->preload($wa->getAsset('style', 'fontscheme.current')->getUri() . '?' . $this->getMediaVersion(), ['as' => 'style']);
+  }
+}
+
+// Enable assets
+$wa->usePreset('template.cassiopeia.' . ($this->direction === 'rtl' ? 'rtl' : 'ltr'))->useStyle('template.active.language')->registerAndUseStyle($assetColorName, 'global/' . $paramsColorName . '.css')->useStyle('template.user')->useScript('template.user')->addInlineStyle(":root { --hue: 214; --template-bg-linght: #f0f4fb; --template-text-dark: #495057; --template-text-light: #ffffff; --template-link-color: var(--link-color); --template-special-color: #001B4C; $fontStyles }");
+
+// Override 'template.active' asset to set correct ltr/rtl dependency
+$wa->registerStyle('template.active', '', [], [], ['template.cassiopeia.' . ($this->direction === 'rtl' ? 'rtl' : 'ltr')]);
+
+// Logo file or site title param
+if ($this->params->get('logoFile')) {
+  $logo = HTMLHelper::_('image', Uri::root(false) . htmlspecialchars($this->params->get('logoFile'), ENT_QUOTES), $sitename, ['loading' => 'eager', 'decoding' => 'async'], false, 0);
+} elseif ($this->params->get('siteTitle')) {
+  $logo = '<span title="' . $sitename . '">' . htmlspecialchars($this->params->get('siteTitle'), ENT_COMPAT, 'UTF-8') . '</span>';
+} else {
+  $logo = HTMLHelper::_('image', 'logo.svg', $sitename, ['class' => 'logo d-inline-block', 'loading' => 'eager', 'decoding' => 'async'], true, 0);
+}
+
+$hasClass = '';
+
+if ($this->countModules('sidebar-left', true)) {
+  $hasClass .= ' has-sidebar-left';
+}
+
+if ($this->countModules('sidebar-right', true)) {
+  $hasClass .= ' has-sidebar-right';
+}
+
+// Container
+$wrapper = $this->params->get('fluidContainer') ? 'wrapper-fluid' : 'wrapper-static';
+
+$this->setMetaData('viewport', 'width=device-width, initial-scale=1.0');
+
+$stickyHeader = $this->params->get('stickyHeader') ? 'position-sticky sticky-top' : '';
+
+// Defer fontawesome for increased performance. Once the page is loaded javascript changes it to a stylesheet.
+$wa->getAsset('style', 'fontawesome')->setAttribute('rel', 'lazy-stylesheet');
+?>
+<!DOCTYPE html>
+<html lang="<?php echo $this->language; ?>" dir="<?php echo $this->direction; ?>">
+  <head>
+    <jdoc:include type="metas" />
+    <jdoc:include type="styles" />
+    <jdoc:include type="scripts" />
+  </head>
+  <body class="site <?php echo $option . ' ' . $wrapper . ' view-' . $view . ($layout ? ' layout-' . $layout : 'no-layout') . ($task ? ' task-' . $task : 'no-task') . ($itemid ? ' itemid-' . $itemid : '') . ($pageclass ? ' ' . $pageclass : '') . $hasclass . ($this->direction == 'rtl' ? 'rtl' : ''); ?>">
+    <header></header>
+  </body>
+</html>
